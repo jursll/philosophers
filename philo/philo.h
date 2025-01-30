1e0abdd56161f19506adc6ab297a22e8ff9f34df
Julia Russell <julrusse@c2r6s3.42lausanne.ch>
2025-01-30 16:09:05 +0100

    30.01 16:08 42

---
+++ philo/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julrusse <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 13:57:05 by julrusse          #+#    #+#             */
+/*   Updated: 2025/01/30 15:25:37 by julrusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+
+
+#endif
